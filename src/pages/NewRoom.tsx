152784eb52f387faf527a7834ae66197cd979371
pedro-drosa <pedro_drosa@hotmail.com>
2021-06-22 21:58:46 -0300

    created: navigation with react-router-dom

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import {Link} from 'react-router-dom';
+
 import { Button } from '../components/Button';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -25,7 +27,7 @@ export function NewRoom() {
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
           </p>
         </div>
       </main>
