4a0eafff50c86adf98e704cfba48e293f989df3f
pedro-drosa <pedro_drosa@hotmail.com>
2021-06-24 22:04:35 -0300

    criado: página da sala e componente: RoomCode

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,3 +1,43 @@
+//useParams pega o parâmetro da rota/ código da sala
+import {useParams} from 'react-router-dom';
+
+import {Button} from '../components/Button';
+import {RoomCode} from '../components/RoomCode';
+
+import logoImg from '../assets/images/logo.svg';
+
+import '../styles/room.scss';
+
+type RoomParams = {
+  id: string;
+}
+
 export function Room() {
-  return(<h1>Sala</h1>);
+  const params = useParams<RoomParams>();
+
+  return(
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask"/>
+          <RoomCode code={params.id}/>
+        </div>
+      </header>
+      <main>
+        <div className="room-title">
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+        <form>
+          <textarea
+            placeholder="O que você quer perguntar?"
+          />
+          <div className="form-footer">
+            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }
